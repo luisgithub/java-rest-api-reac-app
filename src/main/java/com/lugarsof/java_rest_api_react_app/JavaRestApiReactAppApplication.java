758d2232101593139a6f324404b2596d04b35fe2
Luis Garcia <lugar.blanco@gmail.com>
2025-04-16 11:22:17 -0700

    add java project

---
+++ src/main/java/com/lugarsof/java_rest_api_react_app/JavaRestApiReactAppApplication.java
@@ -0,0 +1,13 @@
+package com.lugarsof.java_rest_api_react_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestApiReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestApiReactAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lugarsof/java_rest_api_react_app/JavaRestApiReactAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugarsof.java_rest_api_react_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestApiReactAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
